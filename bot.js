a891731a00284831876b17f340bbc7cb85f81ca3
drumms <77141411+drumms@users.noreply.github.com>
2021-03-13 17:29:30 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^530340048$/;
+      botRegex = /^Will Morland$/;
 
-  if(request.user_id && botRegex.test(request.user_id)) {
+  if(request.name && botRegex.test(request.name)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
