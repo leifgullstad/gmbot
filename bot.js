6f74fb22b16a11483944a00c9c0489b171be09c2
drumms <77141411+drumms@users.noreply.github.com>
2021-03-13 17:18:58 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+  var request = JSON.parse(this.req.user_id),
+      botRegex = /^\530340048$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
