94482458eb4c2bbe77cf9bc018983c66ef018b55
leifgullstad <leifg@umich.edu>
2022-12-14 17:40:56 -0500

    hot fix

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^22686489$/;
 
-  if(true/*request.user_id && botRegex.test(request.user_id)*/) {
+  if(false/*request.user_id && botRegex.test(request.user_id)*/) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
