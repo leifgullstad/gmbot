75114a1a681975c97ab38084cf5c7f4379bf1f54
leifgullstad <leifg@umich.edu>
2022-12-14 18:11:00 -0500

    Testing for name recognition

--- bot.js
+++ bot.js
@@ -3,14 +3,25 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
-function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      leifRegex = /^74942548$/,
-      coleRegex = /^33350163$/;
 
-  if(request.user_id && (leifRegex.test(request.user_id) || coleRegex.test(request.user_id))) {
+var keithRegex = /^72906068$/;
+var kirschRegex = /^66156551$/;
+var timRegex = /^39924012$/;
+var daveRegex = /^71907387$/;
+var leifRegex = /^74942548$/;
+var simonRegex = /^39611527$/;
+
+function getName(user_id) {
+    if(leifRegex.test(user_id)) return "Leif"
+}
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]);
+
+  if (request.user_id && (keithRegex.test(request.user_id) || kirschRegex.test(request.user_id) || leifRegex.test(request.user_id)
+                          || timRegex.test(request.user_id) || daveRegex.test(request.user_id) || simonRegex.test(request.user_id))) {
     this.res.writeHead(200);
-    postMessage(request.name);
+    postMessage(getName(request.user_id));
     this.res.end();
   } else {
     console.log("don't care");
@@ -30,7 +41,7 @@ function postMessage(user_name) {
 
   body = {
     "bot_id" : botID,
-    "text" : user_name + " has a tiny penis"
+    "text" : "Fuck " + user_name + " and fuck EC"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
